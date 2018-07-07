7d5831a3965d1a2b7652361c13efa3b2aeee0049
rachel-rocks-at-ruby <rachel@voytravel.com>
2018-07-07 16:06:28 -0700

    added more testing and truffle-hdwallet-provider module

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -34,6 +34,7 @@ describe('Car', () => {
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!';
 
 beforeEach(async () => {
 	// Get a list of all accounts
@@ -42,7 +43,7 @@ beforeEach(async () => {
 	// Use one of those accounts to deploy
 	// the contract
 	inbox = await new web3.eth.Contract(JSON.parse(interface))
-		.deploy({ data: bytecode, arguments: ['Hi there!'] })
+		.deploy({ data: bytecode, arguments: [INITIAL_STRING] })
 		.send({ from: accounts[0], gas: '1000000' });
 
 	inbox.setProvider(provider);
@@ -51,6 +52,17 @@ beforeEach(async () => {
 describe('Inbox', () => {
 	it('deploys a contract', () => {
 		assert.ok(inbox.options.address);
+	});
+
+	it('has a default message', async () => {
+		const message = await inbox.methods.message().call();
+		assert.equal(message, INITIAL_STRING);
+	});
+
+	it('can change the message', async () => {
+		await inbox.methods.setMessage('Bye').send({ from: accounts[0] });
+		const message = await inbox.methods.message().call();
+		assert.equal(message, 'Bye');
 	});
 });
 
